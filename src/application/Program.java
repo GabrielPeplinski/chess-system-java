39584778e4b32f22949d5af16f119c00fb1fd612
Gabriel Peplinski <gabrielspeplinski2301@gmail.com>
2020-02-20 21:06:31 -0300

    Impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+
+		ChessMatch chess = new ChessMatch();
+		UI.printBoard(chess.getPieces());
 	}
 }
